a3f25be7d15b045bd03babb984aeef5a1a438259
rishikeshtadaka <rishikeshtadaka@gmail.com>
2016-01-24 14:02:18 +0530

    Plant details UI

---
+++ client/views/plantDetails/plantDetails.js
@@ -0,0 +1,26 @@
+﻿var plantDetailsService = CodeBashApp.plantDetailsService.getInstance();
+Templates.searchPlant.Events({
+    "click #searchPlant": function () {
+        var searchKey = $("#searchPlant").val();
+        CodeBashApp.platDetails.plantArray = plantDetailsService.findTree(searchKey);
+    }
+});
+
+Templates.plantDetails.Events({
+
+});
+
+Templates.plantDetails.helpers({
+    getHeaderName: function () {
+        return "Plant Details";
+    }
+});
+
+Templates.plantDetailsTable.events({
+    "click #updatePlant": function () {
+        plantDetailsService.updatePlant();
+    },
+    "click #deletePlant": function () {
+        plantDetailsService.deletePlant();
+    }
+    });
