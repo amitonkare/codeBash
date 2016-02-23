02edabc9eead35384b4a16a24b459ff39c9d120c
adityagholap <adityagholap25@gmail.com>
2016-02-23 22:01:31 +0530

    plant Details code refactored
    Hi team
    updates are as  follows
    1. Confirm box for delete plants.
    2. Update form displays original values in textboxes.
    3. Plant quantity and cost takes only numbers as input.
    4. plant type and category  added through database.
    
    please review the code and please provide fix for following issues
    1.  validation script added but it does not work please review it.
    2 .  datatable reload after adding and deleting plants does not
    work  please review it.
    
    current work in progress.
    1.  Buyer Details  is under progress.
    2. transaction Details is under progress.

--- client/views/plantDetails/plantDetails.js
+++ client/views/plantDetails/plantDetails.js
@@ -2,39 +2,126 @@
     function () 
     {
         $(document).ready(function () {
-        $('#list-plants').DataTable();
-        $(".tool-tip").tooltip();
+            $('#list-plants').DataTable();
+            $(".tool-tip").tooltip();
         });
 
+            $("#newPlantCost,#plantCost").keydown(function(event) {
+    // Allow only backspace and delete
+    if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 190) {
+        // let it happen, don't do anything
     }
-);
+    else {
+        // Ensure that it is a number and stop the keypress
+        if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+            event.preventDefault(); 
+                }   
+            }
+        });
+
+    $("#newPlantQuantity,#plantQuantity").keydown(function(event) {
+    // Allow only backspace and delete
+    if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 ) {
+        // let it happen, don't do anything
+    }
+    else {
+        // Ensure that it is a number and stop the keypress
+        if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+            event.preventDefault(); 
+                }   
+            }
+        });
+
+    //validation code
+    $('#newPlantForm,#editPlantForm').validate({
+    rules: {
+        newPlantName: {
+            minlength: 2,
+            required: true
+        },
+        newPlantScientificName: {
+            minlength: 2,
+            required: true
+        },
+        newPlantComments: {
+            minlength: 2,
+            required: true
+        },
+        newPlantCost: {
+            minlength: 2,
+            required: true
+        },
+        newPlantQuantity: {
+            minlength: 2,
+            required: true
+        },
+        plantName: {
+            minlength: 2,
+            required: true
+        },
+        plantScientificName: {
+            minlength: 2,
+            required: true
+        },
+        plantComments: {
+            minlength: 2,
+            required: true
+        },
+        plantCost: {
+            minlength: 2,
+            required: true
+        },
+        plantQuantity: {
+            minlength: 2,
+            required: true
+        },
+    },
+    highlight: function (element) {
+        $(element).closest('.control-group').removeClass('success').addClass('error');
+    },
+    success: function (element) {
+        element.text('OK!').addClass('valid')
+            .closest('.control-group').removeClass('error').addClass('success');
+    }
+
+
+});
+
+
+
+
+
+
+
+
+
+    }//end of function
+);//end of onRendered
 
 Template.plantDetailsTable.events({
+    "click #dPlant":function()
+    {
+        Session.set('deleteId',this._id);
+    },
     "click #addPlantDetails":function()
     {
         Session.set('add','true');
     },
-    "click #addPlant":function()
+    "click #addPlant":function(event)
     {
-        var addPlantName = $("#addPlantName").val();
-        var addPlantType = $("#addPlantType").val();
-        var addPlantScientificName = $("#addPlantScientificName").val();
-        var addPlantCategory = $("#addPlantCategory").val();
-        var addPlantCost = $("#addPlantCost").val();
-        var addPlantQuantity = $("#addPlantQuantity").val();
-        var addPlantComments = $("#addPlantComments").val();
-        var obj = {};
-        obj.id = Math.random();
-        obj.name = addPlantName;
-        obj.scientificName = addPlantScientificName;
-        obj.comments = addPlantComments;
-        obj.type = addPlantType;
-        obj.category = addPlantCategory;
-        obj.cost = addPlantCost;
-        obj.unit = addPlantQuantity;
-        console.log(obj);
-        CodeBashApp.plantDetailsService.getInstance().addPlant(obj);
-        Session.set('add','');  
+        console.log(event);
+        var plantName = $("#newPlantName").val();
+        var plantType = $("#newPlantType").val();
+        var plantScientificName = $("#newPlantScientificName").val();
+        var plantCategory = $("#newPlantCategory").val();
+        var plantCost = $("#newPlantCost").val();
+        var plantQuantity = $("#newPlantQuantity").val();
+        var plantComments = $("#newPlantComments").val();
+        var plantToBeAdded = CodeBashApp.plantDetailsVO(plantName, plantType, plantScientificName, plantCategory, plantCost, plantQuantity, plantComments);
+        CodeBashApp.plantDetailsService.getInstance().addPlant(plantToBeAdded);
+        Session.set('add',''); 
+        var table = $('#list-plants').DataTable();
+        table.ajax.reload();
     },
     "click #updateDetails":function()
     {
@@ -53,9 +140,12 @@ Template.plantDetailsTable.events({
         CodeBashApp.plantDetailsService.getInstance().updatePlant(Session.get('id'),name,plantScientificName,plantType,plantCategory,plantCost,plantQuantity,plantComments);
         Session.set('update','');
         Session.set('id','');
+        var table = $('#list-plants').DataTable();
+        table.ajax.reload();
     },
     "click #deletePlant": function () {
-        CodeBashApp.plantDetailsService.getInstance().deletePlant(this._id);
+        console.log("delete id -->"+Session.get('deleteId'));
+        CodeBashApp.plantDetailsService.getInstance().deletePlant(Session.get('deleteId'));
     }
 });
 
@@ -67,7 +157,7 @@ Template.plantDetailsTable.helpers({
         }
     },   
     plantsList : function(){
-         if(Session.get('searchKey')) 
+        /*if(Session.get('searchKey')) 
          {              
          plants = CodeBashApp.plantDetailsService.getInstance().findPlantByName(Session.get('searchKey')); 
          return plants;
@@ -76,9 +166,25 @@ Template.plantDetailsTable.helpers({
         {
          plants = CodeBashApp.plantDetailsService.getInstance().findPlantByType(Session.get('searchType')); 
          return plants;   
-        }                  
+        }  */                
         plants = CodeBashApp.plantDetailsService.getInstance().findPlants();
         console.log("plants -- >" + plants);
         return plants;
+    },
+    categoryList:function()
+    {
+        return PlantCategory.find();
+    },
+    typeList : function()
+    {
+        return PlantType.find();
+    },
+    updateObject:function()
+    {
+        if(Session.get('id'))
+        {
+         var obj = CodeBashApp.plantDetailsService.getInstance().findPlantById(Session.get('id'))[0];   
+        }
+        return obj;
     }
 });
